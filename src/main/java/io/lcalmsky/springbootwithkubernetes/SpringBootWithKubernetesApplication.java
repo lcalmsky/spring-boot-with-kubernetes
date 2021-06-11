0d46ca852e7e2bd7eada3ca3f6cab5e95054a8b8
Jaime <lcalmsky@gmail.com>
2021-06-11 18:08:19 +0900

    Initial commit

---
+++ src/main/java/io/lcalmsky/springbootwithkubernetes/SpringBootWithKubernetesApplication.java
@@ -0,0 +1,13 @@
+package io.lcalmsky.springbootwithkubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithKubernetesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithKubernetesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/lcalmsky/springbootwithkubernetes/SpringBootWithKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lcalmsky.springbootwithkubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithKubernetesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
